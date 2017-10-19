18b0137d67e54b68b8dece711f0785b9e2d797cd
Anthony Vallee <tony.vallee@hotmail.com>
2017-10-18 22:21:44 -0400

    Untested version, first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const
+gutil = require("gulp-util"),
+path = require("path"),
+nunjucks = require("nunjucks"),
+through = require("through2");
+
+const PluginError = gutil.PluginError.bind(null, "gulp-swg");
+
+function compile(template) {
+    return through.obj(function(file, enc, callback) {
+        if (file.isNull()) { callback(null, file); return; }
+        if (file.isStream()) {
+            callback(new PluginError("Streaming not supported"));
+            return;
+        }
+        try {
+            const relativePath = path.relative(file.cwd, file.path),
+                  dataFile = relativePath
+                  .replace(/template/, "data")
+                  .replace(/.html/, ".js");
+
+            const data = require(dataFile);
+
+            file.contents = new Buffer(nunjucks.renderString(template, data));
+            this.push(file);
+        } catch (error) {
+            this.emit(new PluginError(err, {filename: file.path}));
+        }
+        callback();
+    });
+}
+
+module.exports = compile;
